bdf0abfc9028aec48ab62bf1ce09f6c1df10ca55
Cesar Marrote Manzano <43895268+cesarmmanzano@users.noreply.github.com>
2019-09-24 19:56:46 -0300

    Add files via upload

--- 2048/src/game_2048/Direction.java
+++ 2048/src/game_2048/Direction.java
@@ -1,5 +1,7 @@
 package game_2048;
 
+//Checa qual direção a peça irá
+
 public enum Direction {
 
     LEFT, RIGHT, UP, DOWN
--- 2048/src/game_2048/Point.java
+++ 2048/src/game_2048/Point.java
@@ -1,9 +1,7 @@
 package game_2048;
 
 public class Point {
-    /*java ja tem uma classe point porem no nosso programa ela pode considerar coluna como x e linha como y o que confunde*/
-    /*essa classe so guarda a posicao certa nas variaveis corretas para nao dar problemas futuros*/
-
+    
     public int row;
     public int col;
 
