197ffbc1dc728a6d6cffc845ae997b3f65410d73
Cesar Marrote Manzano <43895268+cesarmmanzano@users.noreply.github.com>
2019-11-12 16:21:03 -0300

    Add files via upload

--- android/app/src/main/java/org/tensorflow/lite/examples/classification/mainAction.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/classification/mainAction.java
@@ -2,6 +2,7 @@ package org.tensorflow.lite.examples.classification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,25 @@ public class mainAction extends AppCompatActivity {
     }
     //funcao teste
     public void mathwe(View v){
+        Intent intent = new Intent (mainAction.this, CameraActivity.class);
+        startActivity(intent);
+
         System.out.println("Ola galera zou matheus moredi");
     }
 }
+
+    public void upmov(View v){
+        System.out.println("Foi pra cima");
+    }
+
+    public void leftmov(View v){
+        System.out.println("Foi pra esquerda");
+    }
+
+    public void rightmov(View v){
+        System.out.println("Foi pra direita");
+    }
+
+    public void downmov(View v){
+        System.out.println("Foi pra baixo");
+    }
